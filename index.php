f436f4e96c0ebef82577c0929197d67231ef2087
daidevlop <cut0266@gmail.com>
2023-11-20 09:50:13 +0700

    upload_tk_sql

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include 'model/pdo.php';
 include 'model/sanpham.php';
 include 'model/danhmuc.php';
@@ -14,6 +15,16 @@ $list_dm = loadAll_danhmuc();
 if (isset($_GET['act']) && $_GET['act'] != '') {
     $act = $_GET['act'];
     switch ($act) {
+        case 'dangky':
+            if(isset($_POST['dangky'])&&($_POST['dangky']>0)){
+                $user=$_POST['user'];                
+                $email=$_POST['email'];
+                $pass=$_POST['pass'];
+                insert_taikhoan($user,$pass,$email);
+                $thongbao="Đã đăng ký thành công. Vui lòng đăng nhập để thực hiện chức năng bình luận and đặt hàng";
+            }
+                include "view/taikhoan/dangky.php";
+            break;
     }
 } else {
     include 'view/home.php';
---
+++ view/taikhoan/dangky.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="space.css">
+    <script src="https://kit.fontawesome.com/c9f5871d83.js" crossorigin="anonymous"></script>
+    <title>Space</title>
+</head>
+<body>
+    <div class="container">
+        <div class="left">
+            <div class="header">
+                <h2>Chào mừng bạn đến với thế giới đồng hồ</h2>
+                <h4>Please Login </h4>
+            </div>
+            <div class="input">
+                <input type="text" placeholder="user name" required>
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="input">
+                <input type="password" placeholder="password" required>
+                <i class="fa-solid fa-lock"></i>
+            </div>
+            <div class="forget">
+                <label><input type="checkbox"> Remember me</label>
+            </div>
+            <div class="btn">
+                <button>Sign in</button>
+                <span>Bạn chưa có tài khoản? <a href="">Đăng kí</a></span>
+            </div>
+            <div class="with">
+                <div class="fb">
+                    <a href="#"><i class="fa-brands fa-facebook"></i>Facebook</a>
+                </div>
+                <div class="gg">
+                    <a href="#"><i class="fa-brands fa-twitter"></i>Twitter</a>
+                </div>
+            </div>
+        </div>
+        <div class="right">
+            <img src="https://i.pinimg.com/originals/6a/ba/8d/6aba8d6fe7a455389b50d24cfbc84316.jpg">
+        </div>
+    </div>
+</body>
+</html>
