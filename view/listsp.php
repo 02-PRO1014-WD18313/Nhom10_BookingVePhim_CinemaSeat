8231e43af48e31da5ff7b8e78870619e4205af8d
datgin <dnguyentien145@gmail.com>
2023-11-22 11:11:11 +0700

    code lốt layout sanpham 22/11(11:11)

--- view/listsp.php
+++ view/listsp.php
@@ -1,15 +1,36 @@
-<main class="box-product">
+<main class="box-productt">
     <div class="filter">
+        <form action="" method="post">
+            <h4>Lọc giá</h4>
+            <div class="filter-price">
+                <input type="number" placeholder="0" name="min"> -> <input type="number" placeholder="3000000" name="max">
+            </div>
+            <h4>Kiểu máy</h4>
+            <div style="overflow: auto; height: 213px;" class="filter-price">
+                <input name="a" type="radio"> Hợp kim titan <br>
+                <input name="a" type="radio"> Rác vàng <br>
+                <input name="a" type="radio"> Đính kim cương <br>
+                <input name="a" type="radio"> Bọc thép <br>
+                <input name="a" type="radio"> Hợp kimradio titan <br>
+                <input name="a" type="radio"> Rác vàng <br>
+                <input name="a" type="radio"> Đính kim cương <br>
+                <input name="a" type="radio"> Bọc thép <br>
+                <input name="a" type="radio"> Hợp kim titan <br>
+                <input name="a" type="radio"> Rác vàng <br>
+                <input name="a" type="radio"> Đính kim cương <br>
+                <input name="a" type="radio"> Bọc thép <br>
+            </div>
+            <button name="btn" value="btn">Lọc</button>
+        </form>
     </div>
     <div class="box-sp">
-        <form action="?act=listsp" method="post">
-            <select name="filter_price" id="filter_price" onchange="redirectToPage()">
+        <form action="" method="post">
+            <select name="filter_p" id="filter_price">
                 <option hidden value="">Giá</option>
-                <option value="300-500">300 - 500</option>
-                <option value="500-1tr">500 - 1tr</option>
-                <option value="1tr-3tr">1tr - 3tr</option>
-                <option value="3tr-5tr">3tr - 5tr</option>
+                <option value="Giảm dần">Giảm dần</option>
+                <option value="Tăng dần">Tăng dần</option>
             </select>
+            <button name="btn" value="btn">Lọc</button>
         </form>
         <div class="block mt listsp">
             <?php
@@ -41,9 +62,9 @@
         </div>
     </div>
 </main>
-<script>
+<!-- <script>
     function redirectToPage() {
     // Chuyển hướng đến trang chủ
     window.location.href = "?act=listsp";
   }
-</script>
+</script> -->
